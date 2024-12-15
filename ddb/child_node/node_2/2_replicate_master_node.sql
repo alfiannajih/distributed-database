ae9aefdab5e8e0a9689ce4424ca7672a051900d2
alfiannajih <alfiannajih@gmail.com>
2024-12-15 14:11:39 +0700

    update

--- ddb/child_node/node_2/2_replicate_master_node.sql
+++ ddb/child_node/node_2/2_replicate_master_node.sql
@@ -1,21 +1,21 @@
 SELECT PG_SLEEP(4);
 
--- Replicate barang from master-node
+-- Replicate barang from 10.42.0.1
 CREATE SUBSCRIPTION barang_sub_2
-CONNECTION 'host=master-node port=5432 dbname=postgres password=postgres'
+CONNECTION 'host=10.42.0.1 port=5432 dbname=postgres password=postgres'
 PUBLICATION barang_pub;
 
--- Replicate kategori_barang from master-node
+-- Replicate kategori_barang from 10.42.0.1
 CREATE SUBSCRIPTION kategori_barang_sub_2
-CONNECTION 'host=master-node port=5432 dbname=postgres password=postgres'
+CONNECTION 'host=10.42.0.1 port=5432 dbname=postgres password=postgres'
 PUBLICATION kategori_barang_pub;
 
--- Replicate customer from master-node
+-- Replicate customer from 10.42.0.1
 CREATE SUBSCRIPTION customer_sub_2
-CONNECTION 'host=master-node port=5432 dbname=postgres password=postgres'
+CONNECTION 'host=10.42.0.1 port=5432 dbname=postgres password=postgres'
 PUBLICATION customer_pub;
 
--- Replicate supplier from master-node
+-- Replicate supplier from 10.42.0.1
 CREATE SUBSCRIPTION supplier_sub_2
-CONNECTION 'host=master-node port=5432 dbname=postgres password=postgres'
+CONNECTION 'host=10.42.0.1 port=5432 dbname=postgres password=postgres'
 PUBLICATION supplier_pub;
